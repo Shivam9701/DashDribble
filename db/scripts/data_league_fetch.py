93b4cd69fb2489dddd7e17e990e63d4206ae9561
Shivam9701 <shivamkichitthi@gmail.com>
2025-01-26 07:00:40 +0000

    Created Database for T5 Leauges Current & Historical

---
+++ db/scripts/data_league_fetch.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+API_URL = os.getenv("FOOTBALL_DATA_ORG_URL")
+API_TOKEN = os.getenv("FOOTBALL_DATA_ORG_TOKEN")
+
+competitions = [  # "PL",
+    "PD",
+    "SA",
+    "BL1",
+    "FL1",
+]
+
+for competition in competitions:
+    response = requests.get(
+        f"{API_URL}{competition}", headers={"X-Auth-Token": API_TOKEN}
+    )
+    data = response.json()
+    with open(f"data/{competition}.json", "w") as f:
+        json.dump(data, f)
